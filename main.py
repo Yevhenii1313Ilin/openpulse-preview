3288ae95416259469d45677b33093ddc63c61d04
Yevhenii Ilin <ilyin.apple@icloud.com>
2025-05-04 05:30:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.default import DefaultBotProperties
+from aiogram.filters import Command
 from dotenv import load_dotenv
 from handlers.handle_preview import router as preview_router
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-from aiogram.client.default import DefaultBotProperties
-
 bot = Bot(
     token=BOT_TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN)
 )
-
 dp = Dispatcher(storage=MemoryStorage())
 dp.include_router(preview_router)
 
-@dp.message(commands=["start"])
+@dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer("👋 Welcome to OpenPulse Preview. Type /preview to begin.")
 
